0d58a85ad11981499eb1ad1bbeb6be8136671152
Richard Manzoli <manzolirch@gmail.com>
2018-06-01 22:22:03 -0300

    Timer Function

--- src/App.js
+++ src/App.js
@@ -33,8 +33,16 @@ class App extends Component {
     clearInterval(this.counterTimer)
   }
 
+  timer(seconds) {
+    const minutesPart = Math.floor(seconds / 60)
+    const secondsPart = seconds - minutesPart * 60
+    return `${minutesPart}:${secondsPart}`
+  }
+
   getValue(e) {
-    this.value = e.target.value * 60
+    this.setState({
+      value: this.timer(e.target.value)
+    })
   }
 
   render() {
